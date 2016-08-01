f9197072e1aaf88e1a0da2878264bff3e8c05250
Max Neuvians <max@neuvians.net>
2016-08-01 14:28:29 -0400

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule NioGoogleAuthenticator.Mixfile do
 
   def project do
     [app: :nio_google_authenticator,
-     version: "2.0.0",
+     version: "2.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
